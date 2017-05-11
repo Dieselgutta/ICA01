fac09a5267098eca0daa7e332317b1af01f7a3ba
simenffredriksen <sffred16@student.uia.no>
2017-05-11 15:19:09 +0200

    Simen endringer

--- 1.2.4/hello.go
+++ 1.2.4/hello.go
@@ -3,5 +3,6 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World, From Rasmus")
         fmt.Println("Hello World, From Kristian")
-        fmt.Println("Hello World, From Jone)
+        fmt.Println("Hello World, From Jone")
+        fmt.Println("Hello World, From Simen")
 }
